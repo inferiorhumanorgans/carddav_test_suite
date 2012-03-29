cef1e74f048364374853848da301223edf01c0ed
Alex Zepeda <alex@inferiorhumanorgans.com>
2012-03-29 11:33:45 -0700

    Rejigger this so that we check for the proper response to an invalid property (400, not 207).

--- spec/fuzz/principal_fuzz_spec.rb
+++ spec/fuzz/principal_fuzz_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "Principal Resource" do
   require_default_auth
   
-  it "should handle a bogus propfind w/o a 500 error" do
+  it "should handle a bogus propfind like a bad request" do
 
     xml = Nokogiri::XML::Builder.new
     xml.propfind('xmlns:D' => 'DAV:', 'xmlns:C' => 'urn:ietf:params:xml:ns:carddav') do
@@ -12,7 +12,9 @@ describe "Principal Resource" do
       end
     end
 
-    do_propfind(:first, xml)
-    @response.should be_kind_of(Nokogiri::XML::Document)
+    # This is probably really implementation specific.
+    expect {do_propfind(xml, :principals => :first)}.to raise_exception(Net::HTTPServerException) do |ex|
+      ex.data.should be_kind_of Net::HTTPBadRequest
+    end
   end
 end
