cb3c595b81ffbfe3c0d3f15f488cf73079e6f058
Alex Zepeda <alex@inferiorhumanorgans.com>
2012-03-29 15:15:24 -0700

    Allow us to specify a specific namespace for the property.
    Also define Apple's namespace.

--- spec/support/propfind_class_macros.rb
+++ spec/support/propfind_class_macros.rb
@@ -1,13 +1,14 @@
 module PropfindClassMacros
 
-  def setup_single_property(prop)
+  def setup_single_property(prop, prop_ns='C')
     before(:all) do
       @prop = prop
+      @namespaces = {'xmlns:D' => 'DAV:', 'xmlns:C' => 'urn:ietf:params:xml:ns:carddav', 'xmlns:APPLE1' => 'http://calendarserver.org/ns/'}
       @xml = Nokogiri::XML::Builder.new do |xml|
-        xml.propfind('xmlns:D' => 'DAV:', 'xmlns:C' => 'urn:ietf:params:xml:ns:carddav') do
+        xml.propfind(@namespaces) do
           xml.parent.namespace = xml.parent.namespace_definitions.find{|ns|ns.prefix=="D"}
           xml['D'].prop do
-            xml['C'].send prop
+            xml[prop_ns].send prop
           end
         end
       end
