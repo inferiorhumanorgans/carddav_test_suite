eed52d5b3a5761324d1c249388e9020aeecd0a38
Alex Zepeda <alex@inferiorhumanorgans.com>
2012-04-02 15:27:05 -0700

    Make sure that addressbooks are both collections and addresbooks.

---
+++ spec/carddav/5.2_address_book_collections_spec.rb
@@ -0,0 +1,31 @@
+=begin
+5.2. Address Book Collections
+  Address book collections appear to clients as a WebDAV collection
+  resource, identified by a URL.  An address book collection MUST
+  report the DAV:collection and CARDDAV:addressbook XML elements in the
+  value of the DAV:resourcetype property.
+=end
+
+require 'spec_helper'
+
+describe "PROPFIND resourcetype" do
+  require_default_auth
+  setup_single_property('resourcetype', :ns_prefix => 'D')
+  do_propfind(:addressbooks => :mine, :depth => 0)
+
+  it "should return a well-formatted response" do
+    check_propget_one(@prop, :xsl_prefix => 'addressbook_')
+  end
+
+  it "should return the correct href" do
+    response_href_xpath = "#{@response_xpath}/#{@dav_ns}href"
+
+    # This is ugly, but I suppose not including a trailing slash isn't a cardinal sin
+    @response.xpath(response_href_xpath).text.should be_similar_to_href(@addressbook[:href])
+  end
+  
+  it "should return a succesful result" do
+    status_xpath = "#{@response_xpath}/#{@dav_ns}propstat/#{@dav_ns}status"
+    @response.xpath(status_xpath).text.should be_success
+  end
+end
