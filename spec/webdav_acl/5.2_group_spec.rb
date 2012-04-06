7126ee1afc7bf49563785a16d82f92cbc18c6038
Alex Zepeda <alex@inferiorhumanorgans.com>
2012-04-05 21:46:46 -0700

    Refactor the ACL specs.

--- spec/webdav_acl/5.2_group_spec.rb
+++ spec/webdav_acl/5.2_group_spec.rb
@@ -14,46 +14,39 @@
 
 require 'spec_helper'
 
-describe "PROPFIND DAV:group (principal resource)" do
-  require_default_auth
-  setup_single_property('group', :ns_prefix => 'D')
-  do_propfind(:principals => :first, :depth => 0)
+[:principal, :addressbook].each do |resource_type|
 
-  it "should return a well-formatted response" do
-    check_propget_one(@prop)
-  end
+  describe "PROPFIND DAV:group (#{resource_type} resource)" do
+    require_default_auth
+    setup_single_property('group', :ns_prefix => 'D')
 
-  it "should return the correct href" do
-    response_href_xpath = "#{@response_xpath}/#{@dav_ns}href"
+    case resource_type
+    when :principal then
+      do_propfind(:principals => :first, :depth => 0)
+      before(:all) do
+        @object = @principal
+      end
+    when :addressbook
+      do_propfind(:addressbooks => :mine, :depth => 0)
+      before(:all) do
+        @object = @addressbook
+      end
+    end
 
-    # This is ugly, but I suppose not including a trailing slash isn't a cardinal sin
-    @response.xpath(response_href_xpath).text.should be_similar_to_href(@principal[:href])
-  end
-  
-  it "should return a succesful result" do
-    status_xpath = "#{@response_xpath}/#{@dav_ns}propstat/#{@dav_ns}status"
-    @response.xpath(status_xpath).text.should be_success
-  end
-end
+    it "should return a well-formatted response" do
+      check_propget_one(@prop)
+    end
 
-describe "PROPFIND DAV:group (addressbook resource)" do
-  require_default_auth
-  setup_single_property('group', :ns_prefix => 'D')
-  do_propfind(:addressbooks => :mine, :depth => 0)
+    it "should return the correct href" do
+      response_href_xpath = "#{@response_xpath}/#{@dav_ns}href"
 
-  it "should return a well-formatted response" do
-    check_propget_one(@prop)
-  end
+      # This is ugly, but I suppose not including a trailing slash isn't a cardinal sin
+      @response.xpath(response_href_xpath).text.should be_similar_to_href(@object[:href])
+    end
 
-  it "should return the correct href" do
-    response_href_xpath = "#{@response_xpath}/#{@dav_ns}href"
-
-    # This is ugly, but I suppose not including a trailing slash isn't a cardinal sin
-    @response.xpath(response_href_xpath).text.should be_similar_to_href(@addressbook[:href])
-  end
-  
-  it "should return a succesful result" do
-    status_xpath = "#{@response_xpath}/#{@dav_ns}propstat/#{@dav_ns}status"
-    @response.xpath(status_xpath).text.should be_success
+    it "should return a succesful result" do
+      status_xpath = "#{@response_xpath}/#{@dav_ns}propstat/#{@dav_ns}status"
+      @response.xpath(status_xpath).text.should be_success
+    end
   end
 end
